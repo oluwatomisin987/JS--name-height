c62cb312210d7168aa87a75c0cd1182326293af7
OLUWATOMISIN <hardex.tommy@gmail.com>
2022-06-24 17:45:47 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstname= 'Oluwatomisin';
+var middlename= 'Adegoke';
+var lastname= 'Adesanya';
+var height= "5' 6ft";
+var country= "Nigerian";
+
+name= firstname + ' ' + middlename + ' ' + lastname;
+
+document.write('Name:' + ' '+ name + '<br/>');
+document.write('Height:' + ' '+ height + '<br/>');
+document.write('Country:' + ' '+ country + '<br/>');
